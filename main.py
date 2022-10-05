f915b817d634d97146bf57b2feca1440bdbca559
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2022-10-05 22:58:40 +0300

    content editing

--- main.py
+++ main.py
@@ -3,22 +3,53 @@ import random
 import time
 
 tk = Tk()
-tk.title('Игра')
+tk.title("Game")
 tk.resizable(0, 0)
-tk.wm_attributes('-topmost', 1)
-canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
-# canvas.mainloop()
 tk.update()
 
+class Lose_window:
+    def __init__(self, canvas, font, color):
+        self.canvas = canvas
+        self.id = canvas.create_text(250, 100,
+                                     text='ВЫ ПРОИГРАЛИ!',
+                                     font=font,
+                                     fill=color)
+        # self.canvas.move(self.id, 245, 100)
+        # starts = [-3, -2, -1, 1, 2, 3]
+        # random.shuffle(starts)
+        # self.x = starts[0]
 
-class Ball():
+    def draw(self):
+        self.canvas.move(self.id, 0, 0)
+
+
+
+class Start_window:
+    def __init__(self, canvas, font, color):
+        self.canvas = canvas
+        self.id = canvas.create_text(265, 350,
+                                     text='                 нажмите ЛКМ\n'
+                                          'для управления используйте ⬅️⬆️⬇️➡️' ,
+                                     font=font,
+                                     fill=color)
+
+
+    def draw(self):
+        self.canvas.move(self.id, 0, 0)
+
+    def delete(self):
+        self.canvas.move(self.id, 500, 500)
+
+
+class Ball:
     def __init__(self, canvas, paddle, color):
         self.canvas = canvas
         self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        # self.canvas.mainloop()
         starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
@@ -29,18 +60,19 @@ class Ball():
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
-
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
-            self.y = 3
+            self.y = 4
         if pos[3] >= self.canvas_height:
             self.hit_bottom = True
+        if pos[3] >= self.canvas_height:
+            self.y = -4
         if self.hit_paddle(pos) == True:
-            self.y = -3
+            self.y = -4
         if pos[0] <= 0:
-            self.x = 3
+            self.x = 4
         if pos[2] >= self.canvas_width:
-            self.x = -3
+            self.x = -4
 
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
@@ -49,7 +81,8 @@ class Ball():
                 return True
         return False
 
-class Paddle():
+
+class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
@@ -59,12 +92,6 @@ class Paddle():
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
-    def turn_left(self, evt):
-        self.x = -4
-
-    def turn_right(self, evt):
-        self.x = 4
-
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
         pos = self.canvas.coords(self.id)
@@ -73,14 +100,40 @@ class Paddle():
         elif pos[2] >= self.canvas_width:
             self.x = 0
 
+    def turn_left(self, evt):
+        self.x = -4
+
+    def turn_right(self, evt):
+        self.x = 4
+
+
+
+    start = Start_window(canvas, "'Bold' 10", 'black')
+
+
+
+def starting_game(event):
+    while 1:
+        if ball.hit_bottom == False:
+            start = Start_window(canvas, "'Bold' 10", 'white')
+
+            ball.draw()
+            paddle.draw()
+        elif ball.hit_bottom == True:
+            lose = Lose_window(canvas, "'Archangelsk' 30", 'red')
+
+        tk.update_idletasks()
+        tk.update()
+        # lose.draw()
+        time.sleep(0.0001)
+
+
+
+
+
+
 paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, paddle, 'red')
 
-
-while 1:
-    if ball.hit_bottom == False:
-        ball.draw()
-    paddle.draw()
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.01)
+canvas.bind_all('<Button-1>', starting_game)
+canvas.mainloop()
